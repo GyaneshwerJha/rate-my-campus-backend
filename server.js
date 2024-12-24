e4c1f4bd20ba1c7ae3e21f9f39f5c6e4e6b4bae3
GyaneshwerJha <keshavk72411@gmail.com>
2024-12-24 22:44:49 +0530

    env updated

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.use('/api/campuses/:id/reviews', reviewRoutes);
 app.use('/api/campuses', campusRoutes);
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
-    )
-  );
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/dist')));
+//   app.get('*', (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
+//     )
+//   );
+// }
 
 app.use(errorHandler);
 
